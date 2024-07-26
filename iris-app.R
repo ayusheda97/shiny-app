1a2642ea092a80433e85976429a8464704664ee6
ayusheda97 <ayush.heda@ucdconnect.ie>
2024-07-26 02:33:36 +0100

    final update

--- iris-app.R
+++ iris-app.R
@@ -1,104 +1,39 @@
-# Load the Shiny package
+# Load necessary packages
 library(shiny)
 
-# Define UI for iris dataset viewer app
+# Define UI for the app
 ui <- fluidPage(
-  
-  # App title
   titlePanel("Iris Dataset"),
-  
-  # Short description
-  h4("This app visualizes the relationship between Sepal Length and other variables in the Iris dataset, with options to include or exclude outliers."),
-  
-  # Sidebar layout with input and output definitions ----
   sidebarLayout(
-    
-    # Sidebar panel for inputs
     sidebarPanel(
-      
-      # Input: Numeric input for number of rows to display ----
-      numericInput(inputId = "rows",
-                   label = "Number of rows to display:",
-                   value = 5,
-                   min = 1,
-                   max = nrow(iris)),
-      
-      # Input: Selector for variable to plot against Sepal.Length ----
-      selectInput(inputId = "variable",
-                  label = "Variable for Scatter Plot:",
-                  choices = names(iris)[-1]),
-      
-      # Input: Selector for variable to plot histogram ----
-      selectInput(inputId = "histVariable",
-                  label = "Variable for Histogram:",
-                  choices = names(iris)[-1]),
-      
-      # Input: Checkbox for whether outliers should be included ----
-      checkboxInput(inputId = "outliers",
-                    label = "Show outliers",
-                    value = TRUE)
-      
+      numericInput("rows", "Number of rows to display:", value =5, min = 1, max = nrow(iris)),
+      selectInput("variable", "Variable for Scatter Plot:", choices = names(iris)[-5]),
+      selectInput("histVariable", "Variable for Histogram:", choices = names(iris)[sapply(iris, is.numeric)]),
     ),
-    
-    # Main panel for displaying outputs ----
     mainPanel(
-      
-      # Output: Data table ----
-      tableOutput(outputId = "dataTable"),
-      
-      # Output: Scatter Plot ----
-      plotOutput(outputId = "scatterPlot"),
-      
-      # Output: Histogram ----
-      plotOutput(outputId = "histPlot")
-      
+      tableOutput("dataTable"),
+      plotOutput("scatterPlot"),
+      plotOutput("histPlot")
     )
   )
 )
 
-# Define server logic to plot various variables against Sepal.Length ----
+# Define server logic
 server <- function(input, output) {
-  
-  # Data table of the iris dataset
   output$dataTable <- renderTable({
     head(iris, n = input$rows)
   })
   
-  # Scatterplot of the selected variable against Sepal.Length
   output$scatterPlot <- renderPlot({
-    
     x <- iris[[input$variable]]
     y <- iris$Sepal.Length
-    
-    plot(x, y,
-         xlab = input$variable,
-         ylab = "Sepal Length",
-         main = paste("Sepal Length vs", input$variable),
-         pch = 19,
-         col = "blue")
-    
-    # If the checkbox is checked, add outliers
-    if (input$outliers) {
-      points(x, y, pch = 1, col = "red")
-    }
+    plot(x, y, xlab = input$variable, ylab = "Sepal Length", main = paste("Sepal Length vs", input$variable), pch = 19, col = "blue")
   })
   
-  # Histogram of the selected variable
   output$histPlot <- renderPlot({
-    
-    hist(iris[[input$histVariable]],
-         xlab = input$histVariable,
-         main = paste("Histogram of", input$histVariable),
-         col = "lightblue",
-         border = "white")
+    hist(iris[[input$histVariable]], xlab = input$histVariable, main = paste("Histogram of", input$histVariable), col = "lightblue", border = "white")
   })
 }
 
-# Create Shiny app
+# Run the app
 shinyApp(ui = ui, server = server)
-
-
-library(rsconnect)
-rsconnect::deployApp('/Users/ayushheda/Desktop/AdvR/HW3/', appName = 'iris-app')
-
-
